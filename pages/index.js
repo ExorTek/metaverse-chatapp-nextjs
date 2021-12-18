f53072e781b5ce43e89c0e987775b1b4e00c2451
ExorTek <exortek979@gmail.com>
2021-12-18 18:18:10 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Login from "../components/login";
+import {useMoralis} from "react-moralis";
+
+export default function Home() {
+    const {isAuthenticated,logout} = useMoralis();
+    if (!isAuthenticated) return <Login/>;
+    return (
+        <div className="h-screen">
+            <Head>
+                <title>Metaverse Challange</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <h1>Welcome To Metaverse</h1>
+            <button onClick={logout} >Logout</button>
+        </div>
+    );
+}
