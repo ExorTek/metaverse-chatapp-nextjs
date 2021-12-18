3fce314aeed54b5adcd4434fb711215c41ded279
ExorTek <exortek979@gmail.com>
2021-12-18 23:32:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
-import Head from "next/head";
-import Login from "../components/Login";
-import {useMoralis} from "react-moralis";
-
+import Head from 'next/head';
+import Login from '../components/Login';
+import {useMoralis} from 'react-moralis';
+import Header from '../components/Header';
+import Messages from '../components/Messages';
 export default function Home() {
-    const {isAuthenticated,logout} = useMoralis();
+    const {isAuthenticated} = useMoralis();
     if (!isAuthenticated) return <Login/>;
     return (
-        <div className="h-screen">
+        <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900">
             <Head>
-                <title>Metaverse Challange</title>
+                <title>ExorTek Metaverse</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <h1>Welcome To Metaverse</h1>
-            <button onClick={logout} >Logout</button>
+
+                <div className="max-w-screen-2xl mx-auto">
+                    <Header/>
+                    <Messages/>
+                </div>
+
         </div>
     );
 }
