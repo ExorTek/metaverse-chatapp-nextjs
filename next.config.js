d7a00db14bc5a1dadfc576a75db08a7c81212933
ExorTek <exortek979@gmail.com>
2021-12-18 23:32:31 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-        domains: ['image.freepik.com','images.hdqwalls.com','avatars.githubusercontent.com'],
+        domains: ['image.freepik.com','images.hdqwalls.com','images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com','avatars.dicebear.com'],
     },
 };
