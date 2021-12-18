45543448e37c422a5000498a1301299d3c20ecac
ExorTek <exortek979@gmail.com>
2021-12-19 00:41:13 +0300

    Update Messages.js

--- components/Messages.js
+++ components/Messages.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import {useRef} from 'react';
 import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
 import SendMessage from "./SendMessage";
-
+import Message from "./Message";
 
 function Messages() {
+    const MINS_DURATION = 15;
     const {user} = useMoralis();
-    return (
-        <div className="pb-56">
-            <div className="my-5">
-                {/*<ByMoralis variant={"dark"} style={{marginLeft: "auto", marginRight: "auto"}}/>*/}
-            </div>
-            <div>
-
-            </div>
-            <div className="flex justify-center">
-                <SendMessage/>
-            </div>
-            <div className="text-center text-gray-400 mt-5">
-                <p>
-                    You're up to date {user.getUsername()}!
-                </p>
-            </div>
-        </div>
+    const endOfMessageRef = useRef(null);
+    const {data, loading, error} = useMoralisQuery(
+        'Messages',
+        (query) =>
+            query
+                .ascending('createdAt')
+                .greaterThan('createdAt', new Date(Date.now() - 1000 * 60 * MINS_DURATION))
+        , [],
+        {live: true}
     );
+    return (<div className="pb-56 shadow-2xl">
+        <div className="my-5">
+            {/*<ByMoralis variant={"dark"} style={{marginLeft: "auto", marginRight: "auto"}}/>*/}
+        </div>
+        <div className="space-y-10 p-4 ">
+            {data.map(message => (
+                <Message key={message.id} message={message}/>
+            ))}
+        </div>
+        <div className="flex justify-center">
+            <SendMessage endOfMessageRef={endOfMessageRef}/>
+        </div>
+        <div ref={endOfMessageRef} className="text-center text-gray-400 mt-5">
+            <p>
+                You're up to date {user.getUsername()}!
+            </p>
+        </div>
+    </div>);
 }
 
 export default Messages;
