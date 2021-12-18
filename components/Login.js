5156b0278db54da18e423a3b11658c3ba0df607c
ExorTek <exortek979@gmail.com>
2021-12-18 23:32:21 +0300

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -6,18 +6,17 @@ export default function Login() {
 
     return (
         <div className="bg-black relative">
-            <h1>I am Login Screen</h1>
             <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
                 <Image className="object-contain rounded-full"
-                       src="https://avatars.githubusercontent.com/u/54851701?s=400&u=53eb3ce3c5f77c8da8b01580c1811b532f4120ef&v=4"
+                       src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cf2836cb-5893-4a6c-b156-5a89d94fc721/dcap8oj-42db6fc2-99a0-4934-bae0-54116dda7512.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NmMjgzNmNiLTU4OTMtNGE2Yy1iMTU2LTVhODlkOTRmYzcyMVwvZGNhcDhvai00MmRiNmZjMi05OWEwLTQ5MzQtYmFlMC01NDExNmRkYTc1MTIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MenjECatJfuVUD3IfEpWVbwMKCV0w0rXZQMUEF_7BNU"
                        height={200} width={200}/>
                 <button onClick={authenticate}
-                        className="text-white bg-violet-500 rounded-lg p-5 font-bold animate-pulse">Login To Metaverse
+                        className="text-white bg-violet-500 rounded-lg p-5 font-bold animate-pulse">Login To MetaMask
                 </button>
             </div>
             <div className="w-full h-screen">
                 <Image
-                    src="https://images.hdqwalls.com/download/future-metropolis-pn-2880x1800.jpg"
+                    src="https://images.hdqwalls.com/download/futuristic-city-dark-evening-rain-4k-o8-3840x2400.jpg"
                     layout="fill"
                     objectFit="cover"
                 />
